153cb6ec49971e6f2692bc6a7805925493a51a4c
Krzysztof Kogut <krzysiek@jkogut.com>
2020-08-13 18:27:48 +0200

    ready app

---
+++ BitChange/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  BitChange
+//
+//  Created by Kris on 13/08/2020.
+//  Copyright © 2020 Kris. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
